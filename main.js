a055d86086d0a959aba0980f04fc1bbaa3b9d180
quando <quandtm0610@gmail.com>
2025-03-11 10:16:02 +0700

    add condition show hide

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
 // Lấy phần tử popup
 const popupOverlay = document.getElementById("popupOverlay");
 
-// Sau 2 giây, hiển thị popup
-setTimeout(function () {
-    popupOverlay.classList.add("show");
-}, 500);
+// Lấy giờ hiện tại
+const currentHour = new Date().getHours();
+
+// Kiểm tra nếu không nằm trong khoảng 4h sáng - 7h tối thì hiển thị popup
+if (currentHour < 4 || currentHour >= 19) {
+    setTimeout(function () {
+        popupOverlay.classList.add("show");
+    }, 500);
+}
