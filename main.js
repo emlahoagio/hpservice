25fcf646262853aed608fc8cd9be1658db03b441
quando <quandtm0610@gmail.com>
2025-03-12 20:31:08 +0700

    show pop

--- main.js
+++ main.js
@@ -1,12 +1,6 @@
-// Lấy phần tử popup
 const popupOverlay = document.getElementById("popupOverlay");
 
-// Lấy giờ hiện tại
-const currentHour = new Date().getHours();
 
-// Kiểm tra nếu không nằm trong khoảng 4h sáng - 7h tối thì hiển thị popup
-if (currentHour < 4 || currentHour >= 19) {
-    setTimeout(function () {
-        popupOverlay.classList.add("show");
-    }, 500);
-}
+setTimeout(function () {
+    popupOverlay.classList.add("show");
+}, 500);
